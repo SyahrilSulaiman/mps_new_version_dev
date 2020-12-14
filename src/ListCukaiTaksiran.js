887c84c8afafd3a112aed7260ac17c27f59ac16f
wanasyraf96 <wanasyraf96@yahoo.com>
2020-12-14 21:51:08 +0800

    Update ListCukaiTaksiran.js

--- src/ListCukaiTaksiran.js
+++ src/ListCukaiTaksiran.js
@@ -148,7 +148,7 @@ function Bill(props) {
 																								handleBayarSelected
 																								// disabledButton
 																							}>
-							<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+							<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg cursor-pointer">
 								<div className="flex-auto p-3">
 									<div className="flex flex-wrap">
 										<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
@@ -175,7 +175,7 @@ function Bill(props) {
 																								handleBayarSemua
 																								// disabledButton
 																							}>
-							<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
+							<div className="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg cursor-pointer">
 								<div className="flex-auto p-3">
 									<div className="flex flex-wrap">
 										<div className="relative w-full pr-4 max-w-full flex-grow flex-1">
