c55c8561f6e072c90127c6c94c53edbd3de92b68
wanasyraf96 <wanasyraf96@yahoo.com>
2020-12-16 19:13:06 +0800

    commit

--- src/admin/report/Laporan_Carian_Cukai.js
+++ src/admin/report/Laporan_Carian_Cukai.js
@@ -41,8 +41,8 @@ export default function SenaraiCukai({result,type}){
                 <tr>
                     <th className=" bg-blue-100 border text-xs text-center px-2">Bil</th>
                     {/* <th className=" bg-blue-100 border text-left px-8 py-4">Emel</th> */}
-                    <th className=" bg-blue-100 border text-sm text-center px-4 py-2">Nama</th>
-                    <th className=" bg-blue-100 border text-sm text-center px-4 py-2">No Tel</th>
+                    <th className=" bg-blue-100 border hidden md:table-cell text-sm text-center px-4 py-2">Nama</th>
+                    <th className=" bg-blue-100 border md:table-cell text-sm text-center px-4 py-2">No Tel</th>
                     <th className=" bg-blue-100 border text-sm text-center px-4 py-2">
                         {/* {
                             type === 'akaun' ? 'No Akaun':
@@ -51,9 +51,9 @@ export default function SenaraiCukai({result,type}){
                         No Akaun
                     </th>
 
-                    <th className=" bg-blue-100 border text-sm text-center px-4 py-2">No Invois</th>
-                    <th className=" bg-blue-100 border text-sm text-center px-4 py-2">No Resit</th>
-                    <th className=" bg-blue-100 border text-sm text-center px-4 py-2" onClick={handleSort}>Tarikh Pembayaran&nbsp;
+                    <th className=" bg-blue-100 border hidden lg:table-cell text-sm text-center px-4 py-2">No Invois</th>
+                    <th className=" bg-blue-100 border hidden md:table-cell text-sm text-center px-4 py-2">No Resit</th>
+                    <th className=" bg-blue-100 border hidden lg:table-cell text-sm text-center px-4 py-2" onClick={handleSort}>Tarikh Pembayaran&nbsp;
                         {
                             // !isSort?<i className="fas fa-sort"></i>:(sort === 'desc' ? (<i className="fas fa-caret-down"></i>) : sort === 'asc' ? (<i className="fas fa-caret-up"></i>) : '')
                         }
@@ -67,7 +67,6 @@ export default function SenaraiCukai({result,type}){
                 }
                 </tbody>
             </table>
-
             {
                 <Pagination resultPerPage={resultPerPage} totalResult={result.length} paginate={paginate} />
             }
--- src/admin/report/Senarai_Carian_Cukai.js
+++ src/admin/report/Senarai_Carian_Cukai.js
@@ -8,12 +8,12 @@ export default function SenaraiCarianCukai({result, currentPage, resultPerPage, 
             <tr key={index}>
                 <td className="border text-sm text-center py-2">{index+1+((currentPage-1)*resultPerPage)}</td>
                 {/* <td className="border px-8 py-4">{res.email}</td> */}
-                <td className="border text-sm text-center py-2">{res.name}</td>
-                <td className="border text-sm text-center py-2">{res.phone}</td>
+                <td className="border text-sm hidden md:table-cell text-center py-2">{res.name}</td>
+                <td className="border text-sm md:table-cell text-center py-2">{res.phone}</td>
                 <td className="border text-sm text-center py-2">{res.account}</td>
-                <td className="border text-sm text-center py-2">{res.invoice}</td>
-                <td className="border text-sm text-center py-2">{res.receipt}</td>
-                <td className="border text-sm text-center py-2">{res.date}</td>
+                <td className="border text-sm hidden lg:table-cell text-center py-2">{res.invoice}</td>
+                <td className="border text-sm hidden md:table-cell text-center py-2">{res.receipt}</td>
+                <td className="border text-sm hidden lg:table-cell text-center py-2">{res.date}</td>
                 <td className="border text-sm text-center py-2">RM&nbsp;{res.amount}</td>
             </tr>
         )
