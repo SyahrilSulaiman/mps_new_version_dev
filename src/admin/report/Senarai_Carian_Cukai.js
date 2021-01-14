be1ac2c3c49fa39e9cede86d05761ace0225a6ce
WaNa <wanasyraf96@yahoo.com>
2021-01-14 23:20:09 +0800

    Update Senarai_Carian_Cukai.js

--- src/admin/report/Senarai_Carian_Cukai.js
+++ src/admin/report/Senarai_Carian_Cukai.js
@@ -1,5 +1,13 @@
 import React,{ useState, useEffect } from 'react'
 
+function capitalizeTheFirstLetterOfEachWord(words) {
+    var separateWord = words.toLowerCase().split(' ');
+    for (var i = 0; i < separateWord.length; i++) {
+       separateWord[i] = separateWord[i].charAt(0).toUpperCase() +
+       separateWord[i].substring(1);
+    }
+    return separateWord.join(' ');
+ }
 export default function SenaraiCarianCukai({result, currentPage, resultPerPage, total}){
     
     return (
@@ -9,7 +17,7 @@ export default function SenaraiCarianCukai({result, currentPage, resultPerPage, 
             <tr key={index}>
                 <td className="border text-sm text-center py-2">{index+1+((currentPage-1)*resultPerPage)}</td>
                 {/* <td className="border px-8 py-4">{res.email}</td> */}
-                <td className="border text-sm md:table-cell text-left py-2">{res.name}</td>
+                <td className="border text-sm md:table-cell text-left py-2">{capitalizeTheFirstLetterOfEachWord(res.name)}</td>
                 <td className="border text-sm hidden md:table-cell text-left py-2">{res.username}</td>
                 <td className="border text-sm text-left py-2">{res.account}</td>
                 <td className="border text-sm hidden lg:table-cell text-left py-2">{res.invoice}</td>
